e0a3441ddf303718f51a87636510765be548176a
Patricia <patricianjuguna084@gmail.com>
2025-06-06 02:00:31 +0300

    Learned about arrays and array functions

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+function removeLastCat() {
+    return cats.slice(0, - 1);
+}
+
+console.log(destructivelyAppendCat('Garfield'));
+console.log(destructivelyPrependCat('True'));
+console.log(destructivelyRemoveLastCat());
+console.log(destructivelyRemoveFirstCat());
+console.log(appendCat('Jules'));
+console.log(prependCat('Tai'));
+console.log(removeFirstCat());
+console.log(removeLastCat());
